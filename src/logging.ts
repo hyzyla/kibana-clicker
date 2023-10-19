c9b95cbbc89feda8c95d3c28077a74fa0eb8c5e9
Yevhenii Hyzyla <y.hyzyla@vchasno.ua>
2023-10-19 22:33:31 +0200

    Add remove logging on prod

--- src/logging.ts
+++ src/logging.ts
@@ -2,9 +2,7 @@ const prefix = "KibanaClicker";
 
 export function log(msg: string, ...args: any[]) {
   const message = `${prefix}: ${msg}`;
-  if (process.env.PLASMO_TAG === "prod") {
-    console.debug(message, ...args);
-  } else if (process.env.PLASMO_TAG === "dev") {
+  if (process.env.PLASMO_TAG === "dev") {
     console.log(message, ...args);
   }
 }
