0a9188cfd80e9e620eaa7281b4deb0e80269adf5
Yevhenii Hyzyla <hyzyla@gmail.com>
2026-02-28 23:15:21 +0100

    Update publishing

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "wxt";
 
 export default defineConfig({
+  modules: ['@wxt-dev/auto-icons'],
   manifest: {
     name: "Kibana Clicker",
     description: "Automation tool for Kibana",
     permissions: ["storage"],
+    host_permissions: ["<all_urls>"],
+    autoIcons: {
+      baseIconPath: "assets/icon.png",
+    },
     browser_specific_settings: {
       gecko: {
         id: "kibana-clicker@hyzyla.dev",
